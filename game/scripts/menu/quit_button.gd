1dfdec3ff686515869593960445ce12da1417dec
Chenam <117447166+LeChenam@users.noreply.github.com>
2024-10-19 21:00:52 +0200

    icone et update menu

--- game/scripts/menu/quit_button.gd
+++ game/scripts/menu/quit_button.gd
@@ -1,10 +1,8 @@
-extends PanelContainer
+extends Button
 
 
 func _ready() -> void:
-	var button = Button.new()
-	button.text = "Quitter"
-	button.pressed.connect(self._button_pressed)
-	add_child(button)
+	self.text = "Quitter"
+	self.pressed.connect(self._button_pressed)
 func _button_pressed():
 	get_tree().quit()
