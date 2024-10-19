26c38164ed2d7cb09bd6faf8df426c3cd40963dd
Chenam <117447166+LeChenam@users.noreply.github.com>
2024-10-20 00:32:22 +0200

    menu + intro

---
+++ game/scripts/menu/intro.gd
@@ -0,0 +1,12 @@
+extends AudioStreamPlayer2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.play()# Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if(self.playing==false):
+		get_tree().change_scene_to_file("res://scenes/debug.tscn")
